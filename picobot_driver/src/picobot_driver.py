2d90da3ef1289488020ea882053e9783f4545bd8
Aljaz Zajc <aljaz.zajc@student.um.si>
2018-11-21 11:06:59 +0100

    First commit

---
+++ picobot_driver/src/picobot_driver.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+import RPi.GPIO as GPIO  # Import the GPIO Library
+import time
+
+# Set the GPIO modes
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+_FREQUENCY = 20 # konstanta, za notranjo uporabo
+
+def _porezi(value,min,max):
+    """Zagotovimo da je vrednost med 0(min) in 100(max) """
+    if value < min:
+       return min
+    elif value > max:
+       return max
+    return value
+
+class Motor:
+
+   def __init__(self, forward_pin, backward_pin):
+      # self._forward_pin = forward_pin
+       #self._backward_pin = backward_pin
+
+       GPIO.setup(forward_pin, GPIO.OUT)
+       GPIO.setup(backward_pin, GPIO.OUT)
+
+       self._forward_pwm = GPIO.PWM(forward_pin, _FREQUENCY)
+       self._backward_pwm = GPIO.PWM(backward_pin, _FREQUENCY)
+
+   def move(self, speed_percent):
+
+        speed = _porezi(abs(speed_percent),0,100)
+
+        #Pozitivno stevilo premakne kolesa naprej, negativno pa nazaj
+        if speed_percent < 0:
+           self._backward_pwm.start(speed)
+           self._forward_pwm.start(0)
+        else:
+           self._forward_pwm.start(speed)
+           self._backward_pwm.start(0)
+
+class Driver:
+
+  def __init__(self):
+
+    rospy.init_node('picobot_driver')
+    self._last_received = rospy.get_time()
+    self._timeout = rospy.get_param('~timeout', 5)
+    self._max_speed = rospy.get_param('~max_speed', 0.1)#potrebujemo za nadzor angularne hitrosti
+    self._wheel_base = rospy.get_param('~wheel_base', 0.091)
+
+    # Nastavimo pine za motorje
+    self._left_motor = Motor(35, 37)
+    self._right_motor = Motor(36, 38)
+    self._left_speed_percent = 0
+    self._right_speed_percent = 0
+
+    # Subscriber za twist sporocila
+    rospy.Subscriber('/pico/cmd_vel',Twist, self.velocity_received_callback)
+
+
+  def velocity_received_callback(self, message):
+      """Obdelamo ukaze za hitrost"""
+      self._last_received = rospy.get_time()
+
+       # Dobimo linearne in angularne hotrosti
+      linear = message.linear.x
+      angular = message.angular.z
+
+       # Izracunamo vrtenje koles v m/s
+      left_speed = linear - (angular/self._max_speed) * self._wheel_base/2
+      right_speed = linear + (angular/self._max_speed) * self._wheel_base/2
+
+      self._left_speed_percent = (100 * left_speed)
+      self._right_speed_percent = (100 * right_speed)
+
+  def run(self):
+     """ Loop driver-ja """
+
+     rate = rospy.Rate(10)#10 Hz
+
+     while not rospy.is_shutdown():
+     # Ce dolgo ne dobim ukazov potem smo izgubili povezavo
+          delay = rospy.get_time() - self._last_received
+          if delay < self._timeout:
+             self._left_motor.move(self._left_speed_percent)
+             self._right_motor.move(self._right_speed_percent)
+          else:
+             self._left_motor.move(0)
+             self._right_motor.move(0)
+          rate.sleep() 
+
+
+def main():
+    driver = Driver()
+    driver.run()
+
+if __name__ == '__main__':
+     main()
---
+++ picobot_remote_control/src/picobot_remote_control.cpp
@@ -0,0 +1,69 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <geometry_msgs/Twist.h>
+#include "teleop_twist_joy/teleop_twist_joy.h"
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+
+using namespace std;
+
+class PicoRemoteControl
+{
+public:
+  PicoRemoteControl();
+
+private:
+  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+
+  ros::NodeHandle nh_;
+
+  int linear_, angular_;
+  double l_scale_, a_scale_;
+  ros::Publisher vel_pub_;
+  ros::Subscriber joy_sub_;
+};
+
+
+PicoRemoteControl::PicoRemoteControl():
+  linear_(0),//levo,desno 1
+  angular_(1),//naprej,nazaj 0
+  a_scale_(1.5),
+  l_scale_(0.5)
+{
+
+  //nh_.param("axis_linear", linear_, linear_);
+  //nh_.param("axis_angular", angular_, angular_);
+  //nh_.param("scale_angular", a_scale_, a_scale_);
+  //nh_.param("scale_linear", l_scale_, l_scale_);
+
+
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("pico/cmd_vel", 1);
+  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &PicoRemoteControl::joyCallback, this);
+
+}
+
+void PicoRemoteControl::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  geometry_msgs::Twist twist;
+
+  if(joy->buttons[5] > 0){//je RB
+  twist.angular.z = a_scale_*joy->axes[angular_];
+  twist.linear.x = -l_scale_*joy->axes[linear_];
+  vel_pub_.publish(twist);
+  }else{
+  twist.angular.z = 0;
+  twist.linear.x = 0;
+  vel_pub_.publish(twist); 
+  }
+}
+
+
+int main(int argc, char** argv)
+{
+ // sleep(5);
+  ros::init(argc, argv, "picobot_remote_control");
+  PicoRemoteControl pico_remote_control;
+  ros::spin();
+}
+
