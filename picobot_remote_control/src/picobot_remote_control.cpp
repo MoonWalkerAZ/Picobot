0abb1e5f99e54f7da73abeef1044e2b337dce728
Aljaz Zajc <aljaz.zajc@student.um.si>
2019-01-23 16:06:32 +0100

    izgled

--- picobot_remote_control/src/picobot_remote_control.cpp
+++ picobot_remote_control/src/picobot_remote_control.cpp
@@ -16,32 +16,24 @@ public:
 private:
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
- 
-  ros::NodeHandle nh_;
 
-  int lidar_status;
-  int linear_, angular_;
-  double l_scale_, a_scale_;
-  ros::Publisher vel_pub_;
-  ros::Subscriber joy_sub_;
+  ros::NodeHandle n;
+
+  int linear, angular;
+  double linScale, angScale;
+  ros::Publisher velPub;
+  ros::Subscriber joySub;
 };
 
 
 PicoRemoteControl::PicoRemoteControl():
-  linear_(1),//levo,desno 1
-  angular_(0),//naprej,nazaj 0
-  a_scale_(1.8),
-  l_scale_(0.8)
+  linear(1),//levo,desno 1
+  angular(0),//naprej,nazaj 0
+  angScale(1.8),
+  linScale(0.8)
 {
-
-  //nh_.param("axis_linear", linear_, linear_);
-  //nh_.param("axis_angular", angular_, angular_);
-  //nh_.param("scale_angular", a_scale_, a_scale_);
-  //nh_.param("scale_linear", l_scale_, l_scale_);
-
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("pico/cmd_vel", 1);
-  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &PicoRemoteControl::joyCallback, this);
-
+  velPub = n.advertise<geometry_msgs::Twist>("pico/cmd_vel", 1);
+  joySub = n.subscribe<sensor_msgs::Joy>("joy", 10, &PicoRemoteControl::joyCallback, this);
 }
 
 void PicoRemoteControl::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
@@ -50,53 +42,52 @@ void PicoRemoteControl::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
   if(joy->buttons[3] > 0){//Y
     system("cd && rosservice call /start_motor");
     ROS_INFO("Vklaplam motor");
-    }
-else if(joy->buttons[2] > 0){//B
+  }
+  else if(joy->buttons[2] > 0){//B
     system("cd && rosservice call /stop_motor");
     ROS_INFO("Izklapljam motor");
-    }
+  }
 
-if (joy->buttons[4] > 0){//LB
-if (joy->buttons[0] > 0) {//X
-l_scale_+=0.5;
-a_scale_ +=0.5;
-if(l_scale_ >= 1){
-l_scale_ = 0.9;
-}
-if(a_scale_ >= 2){
-a_scale_ = 2;
-}
-}
-if (joy->buttons[1] > 0) {//A
-l_scale_-=0.5;
-a_scale_ -=0.5;
-if(l_scale_ <= 0){
-l_scale_=0.2;
-}
-if(a_scale_ <=0){
-a_scale_ = 0.5;
-}
-}
-}
+  if (joy->buttons[4] > 0){//LB
+    if (joy->buttons[0] > 0) {//X visja hitrost
+      linScale+=0.5;
+      angScale +=0.5;
+      if(linScale >= 1){
+        linScale = 0.9;
+      }
+      if(angScale >= 2){
+        angScale = 2;
+      }
+    }
+    if (joy->buttons[1] > 0) {//A nizja hitrost
+      linScale-=0.5;
+      angScale -=0.5;
+      if(linScale <= 0){
+        linScale=0.2;
+      }
+      if(angScale <=0){
+        angScale = 0.5;
+      }
+    }
+  }
 
 
   geometry_msgs::Twist twist;
 
-  if(joy->buttons[5] > 0){//je RB
-  twist.angular.z = a_scale_*joy->axes[angular_];
-  twist.linear.x = l_scale_*joy->axes[linear_];
-  vel_pub_.publish(twist);
+  if(joy->buttons[5] > 0){//RB varovalo
+    twist.angular.z = angScale*joy->axes[angular];
+    twist.linear.x = linScale*joy->axes[linear];
+    velPub.publish(twist);
   }else{
-  twist.angular.z = 0;
-  twist.linear.x = 0;
-  vel_pub_.publish(twist); 
+    twist.angular.z = 0;
+    twist.linear.x = 0;
+    velPub.publish(twist);
   }
 }
 
 
 int main(int argc, char** argv)
 {
-  //  system("cd && roslaunch /home/pi/catkin_ws/src/Picobot/picobot_imu/launch/picobot_imu.launch");
   ros::init(argc, argv, "picobot_remote_control");
   PicoRemoteControl pico_remote_control;
   ros::spin();
